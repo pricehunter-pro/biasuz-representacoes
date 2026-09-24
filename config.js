fe6f6faebf07e8ef404ed11b70baacc8b96fb11e
pricehunter-pro <falecomchuckit@gmail.com>
2026-09-23 22:36:12 -0300

    chore: set production site URL

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.BIASUZ_CONFIG = {
+  siteUrl: "https://bia.dunihub.online",
   supabaseUrl: "https://hszbmroogcciopipjijk.supabase.co",
   supabasePublishableKey: "sb_publishable_5qQvoQfA8cnVn1GietQcug_RraCvYcH",
   whatsappNumber: ""
