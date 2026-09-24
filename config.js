bfdf7cdb439298a5fedd3116a5b780a16b4f9bd0
pricehunter-pro <falecomchuckit@gmail.com>
2026-09-23 23:03:02 -0300

    chore: configure Biasuz WhatsApp and Instagram

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ window.BIASUZ_CONFIG = {
   siteUrl: "https://bia.dunihub.online",
   supabaseUrl: "https://hszbmroogcciopipjijk.supabase.co",
   supabasePublishableKey: "sb_publishable_5qQvoQfA8cnVn1GietQcug_RraCvYcH",
-  whatsappNumber: ""
+  whatsappNumber: "5575992268989",
+  instagramUrl: "https://www.instagram.com/biasuzrepresentacoes/",
+  instagramHandle: "@biasuzrepresentacoes"
 };
